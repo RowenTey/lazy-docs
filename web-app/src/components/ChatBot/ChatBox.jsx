b1943a008543f0853f6aed3764b3ae5419588d8b
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2023-02-25 21:26:59 +0800

    chatbot ui

---
+++ web-app/src/components/ChatBot/ChatBox.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const ChatBox = ({ setMessages, loading, setLoading }) => {
+	const [query, setQuery] = useState("");
+	const handleSubmit = async () => {
+		if (query.length == 0) {
+			return;
+		}
+		// send request
+		const message = { from: "user", content: query };
+		setLoading(true);
+		setMessages(prevMessage => [message, ...prevMessage]);
+		setQuery("");
+
+		// new loading message
+		setTimeout(() => {
+			const loadingMessage = { from: "chatbot", content: "Loading ..." };
+			setMessages(prevMessage => [loadingMessage, ...prevMessage]);
+		}, 1000);
+
+		// got message
+		setTimeout(() => {
+			// remove last message
+			const newMessage = { from: "chatbot", content: "Answer from api" };
+
+			setMessages(prevMessage => [newMessage, ...prevMessage.slice(1)]);
+		}, 3000);
+	};
+
+	const handleKeyDown = e => {
+		if (e.key === "Enter") {
+			handleSubmit();
+		}
+	};
+
+	return (
+		<div className="w-[96%] flex flex-row items-center justify-center ">
+			<input
+				name="query"
+				type="text"
+				placeholder="Ask me anything..."
+				value={query}
+				className="px-4 py-2 mr-2 rounded-md w-[840px]"
+				onChange={e => setQuery(e.target.value)}
+				onKeyDown={handleKeyDown}
+			/>
+			<button
+				type="submit"
+				onClick={handleSubmit}
+				className="px-4 py-2 bg-blue-200 rounded-md"
+			>
+				Send
+			</button>
+		</div>
+	);
+};
+
+export default ChatBox;
---
+++ web-app/src/components/Poster/Poster.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Poster = () => {
+	return (
+		<div className="bg-slate-500 w-screen h-screen flex justify-center items-center relative"></div>
+	);
+};
+
+export default Poster;
